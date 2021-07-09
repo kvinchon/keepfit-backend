e1fd2df088137c65d5c58ff38a5935bb4e739733
kvinchon <kevin@beevora.com>
2021-07-09 21:40:39 +0200

    fix: workout model

--- models/Workout.js
+++ models/Workout.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, Sequelize) => {
       allowNull: false,
     },
     coordinates: {
-      type: Sequelize.STRING,
+      type: Sequelize.STRING(512),
       allowNull: false,
     },
     startingPoint: {
